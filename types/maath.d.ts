92dc03137283b6842d8396f2a6a3344ab6f23cd1
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2026-02-07 07:56:52 +0600

    --

--- types/maath.d.ts
+++ types/maath.d.ts
@@ -1,15 +1,48 @@
-declare module 'maath/random/dist/maath-random.esm' {
-  type TypedArray = Float32Array | Float64Array | Int8Array | Uint8Array | Uint8ClampedArray | Int16Array | Uint16Array | Int32Array | Uint32Array;
+declare module "maath/random/dist/maath-random.esm" {
+  type TypedArray =
+    | Float32Array
+    | Float64Array
+    | Int8Array
+    | Uint8Array
+    | Uint8ClampedArray
+    | Int16Array
+    | Uint16Array
+    | Int32Array
+    | Uint32Array;
 
-  export function inSphere(buffer: TypedArray, sphere?: { radius?: number; center?: number[] }): TypedArray;
-  export function inBox(buffer: TypedArray, box?: { sides?: number[] | number; center?: number[] }): TypedArray;
-  export function inRect(buffer: TypedArray, rect?: { sides?: number[] | number; center?: number[] }): TypedArray;
-  export function inCircle(buffer: TypedArray, circle?: { radius?: number; center?: number[] }): TypedArray;
-  export function onSphere(buffer: TypedArray, sphere?: { radius?: number; center?: number[] }): TypedArray;
-  export function onBox(buffer: TypedArray, box?: { sides?: number[] | number; center?: number[] }): TypedArray;
-  export function onRect(buffer: TypedArray, rect?: { sides?: number[] | number; center?: number[] }): TypedArray;
-  export function onCircle(buffer: TypedArray, circle?: { radius?: number; center?: number[] }): TypedArray;
-  export function noise: any;
+  export function inSphere(
+    buffer: TypedArray,
+    sphere?: { radius?: number; center?: number[] },
+  ): TypedArray;
+  export function inBox(
+    buffer: TypedArray,
+    box?: { sides?: number[] | number; center?: number[] },
+  ): TypedArray;
+  export function inRect(
+    buffer: TypedArray,
+    rect?: { sides?: number[] | number; center?: number[] },
+  ): TypedArray;
+  export function inCircle(
+    buffer: TypedArray,
+    circle?: { radius?: number; center?: number[] },
+  ): TypedArray;
+  export function onSphere(
+    buffer: TypedArray,
+    sphere?: { radius?: number; center?: number[] },
+  ): TypedArray;
+  export function onBox(
+    buffer: TypedArray,
+    box?: { sides?: number[] | number; center?: number[] },
+  ): TypedArray;
+  export function onRect(
+    buffer: TypedArray,
+    rect?: { sides?: number[] | number; center?: number[] },
+  ): TypedArray;
+  export function onCircle(
+    buffer: TypedArray,
+    circle?: { radius?: number; center?: number[] },
+  ): TypedArray;
+  export const noise: any;
   export class Generator {
     constructor(seed?: any);
     seed: any;
